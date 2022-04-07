2943f690e4a8c70bc5b044718e187976aab3f493
eziomou <eziomou@gmail.com>
2022-04-07 08:08:40 +0200

    Add PageRequest validation

--- src/main/java/io/github/eziomou/pm/endpoint/PageRequest.java
+++ src/main/java/io/github/eziomou/pm/endpoint/PageRequest.java
@@ -1,5 +1,7 @@
 package io.github.eziomou.pm.endpoint;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.ws.rs.DefaultValue;
 import javax.ws.rs.QueryParam;
 
@@ -7,10 +9,13 @@ public class PageRequest {
 
     @QueryParam("page")
     @DefaultValue("0")
+    @Min(value = 0, message = "Page must be greater or equal 0")
     private int page;
 
     @QueryParam("size")
     @DefaultValue("30")
+    @Min(value = 10, message = "Size must be greater or equal 10")
+    @Max(value = 100, message = "Size must be lower or equal 100")
     private int size;
 
     public int getPage() {
--- src/main/java/io/github/eziomou/pm/endpoint/ProjectEndpoint.java
+++ src/main/java/io/github/eziomou/pm/endpoint/ProjectEndpoint.java
@@ -1,14 +1,17 @@
 package io.github.eziomou.pm.endpoint;
 
 import io.github.eziomou.pm.resource.PageResource;
-import io.github.eziomou.pm.model.ProjectEntity;
 import io.github.eziomou.pm.resource.ProjectResource;
 import io.github.eziomou.pm.service.ProjectService;
 import io.smallrye.mutiny.Uni;
 import org.jboss.resteasy.reactive.RestResponse;
 
 import javax.inject.Inject;
-import javax.ws.rs.*;
+import javax.validation.Valid;
+import javax.ws.rs.BeanParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 @Path("/projects")
@@ -19,7 +22,7 @@ public class ProjectEndpoint {
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public Uni<RestResponse<PageResource<ProjectResource>>> getProjects(@BeanParam PageRequest pageRequest) {
+    public Uni<RestResponse<PageResource<ProjectResource>>> getProjects(@Valid @BeanParam PageRequest pageRequest) {
         return projectService.getProjects(pageRequest.getPage(), pageRequest.getSize()).map(RestResponse::ok);
     }
 }
